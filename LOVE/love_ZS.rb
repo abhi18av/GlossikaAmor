6983f7d4acaad510ebf9a6b16a6bb1a93e743796
Abhinav Sharma <abhi18av@gmail.com>
2017-06-22 21:15:11 +0530

    Corrected a couple sentences in the love_ZT

--- LOVE/love_ZS.rb
+++ LOVE/love_ZS.rb
@@ -24,15 +24,17 @@ sentencesQ = sentences.map{ |e| %{"#{e}"} }
 
 len = sentencesQ.length - 1
 
+Dir.mkdir("love_ZS")
 
 for i in (0..len).to_a
 	cmd1 = "trans " + source_lang + ":" + target_lang + " " + sentencesQ[i-1] + ' -no-ansi ' + engineStr
-	puts(cmd1)
+	#puts(cmd1)
 	#system(cmd1)
 
-	#fileName = (i+1).to_s + ".txt"
-	#cmd2 = cmd1 + " > " + fileName
-	#system(cmd2)
+	fileName = "./love_ZS/" + (i+1).to_s + ".txt"
+	puts(i)
+	cmd2 = cmd1 + " > " + fileName
+	system(cmd2)
 	#puts(cmd2)
 end
 
