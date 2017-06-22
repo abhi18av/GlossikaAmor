cbf04bbc6dab316c15068553879ea68842aa2598
Abhinav Sharma <abhi18av@gmail.com>
2017-06-22 22:04:50 +0530

    Working on a script to clean the pinyin and to label sentences.

---
+++ LOVE/love_ZS/addPrefixToSentences.rb
@@ -0,0 +1,19 @@
+
+
+contents = File.read("./joinAllTXT.txt")
+
+
+sentencesAll = contents.split("\n\n")[0..99]
+
+
+def cleanPinyin()
+s[1].length
+s[1][0]
+s[1][-1]
+
+
+
+# to remove the 
+s[1].split("(")[1].split(")")[0]
+
+end
