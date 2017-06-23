1af38608ca408462bed380f5135827333377f2cb
Abhinav Sharma <abhi18av@gmail.com>
2017-06-23 11:23:26 +0530

    Working to create a isolated set of ZH and ZS using some text processing.

--- LOVE/love_ZS/google/addPrefixToSentences.rb
+++ LOVE/love_ZS/google/addPrefixToSentences.rb
@@ -6,14 +6,23 @@ contents = File.read("./joinAllTXT.txt")
 sentencesAll = contents.split("\n\n")[0..99]
 
 
-def cleanPinyin()
-s[1].length
-s[1][0]
-s[1][-1]
+def cleanPinyin(s)
 
-
-
-# to remove the 
-s[1].split("(")[1].split(")")[0]
+	len = s.length
+	x = s.delete(s[0])
+	#puts s[len-1]
+	y = x.delete(s[len-1])
+	return y
 
 end
+
+
+
+for x in sentencesAll
+
+	s = x.split("\n")[1]
+	puts cleanPinyin(s)
+
+#puts s
+
+end 
